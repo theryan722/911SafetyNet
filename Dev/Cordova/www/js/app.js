ee79862ee35351d17c8c6b736928420f801ec6e2
Ryan O'Day <romanguy722@gmail.com>
2017-09-10 02:33:12 -0400

    Finally get the app working!!!!!!!

--- Dev/Cordova/www/js/app.js
+++ Dev/Cordova/www/js/app.js
@@ -35,12 +35,16 @@ var firebaseconfig = {
 firebase.initializeApp(firebaseconfig);
 var isCordovaApp = !!window.cordova;
 if (isCordovaApp) {
-    document.addEventListener("deviceready", loadDialPage(), false);
-    alert('cordova');
+    document.addEventListener("deviceready", onDeviceReady, false);
 } else {
     loadDialPage();
 }
 /* ================ End Initialize ============= */
+
+function onDeviceReady() {
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    loadDialPage();
+}
 /* =========== Load Pages ======== */
 function loadAboutPage() {
     mainView.router.loadPage('pages/about.html');
@@ -52,10 +56,12 @@ function loadDialPage() {
 
 function loadSetInfoPage() {
     mainView.router.loadPage('pages/setinfo.html');
+    loadSettings();
 }
 
 function loadMedicalIDPage() {
     mainView.router.loadPage('pages/medicalid.html');
+    loadMedicalIDInfo();
 }
 /* ============ End Load Pages ======= */
 /* ============ Functions ====== */
@@ -64,22 +70,49 @@ function goBack() {
 }
 
 function loadMedicalIDInfo() {
-    let uinf = JSON.parse(localStorage.getItem('uinfo'));
-    if (uinf) {
-        $$('#u_name').html(uinf.name);
-        $$('#u_phone').html(uinf.phone);
-        $$('#u_birthdate').html(uinf.birthdate);
-        $$('#u_address').html(uinf.address);
-        $$('#u_bloodtype').html(uinf.bloodtype);
-        $$('#u_medications').html(uinf.medications);
-        $$('#u_allergies').html(uinf.allergies);
-        $$('#u_medicalconditions').html(uinf.medicalconditions);
-        $$('#u_doctorinfo').html(uinf.doctorinfo);
-        $$('#u_emergencycontact_name').html(uinf.emergencycontact_name);
-        $$('#u_emergencycontact_phone').html(uinf.emergencycontact_phone);
-        $$('#u_usernotes').html(uinf.usernotes);
-        $$('#u_picture').attr('src', localStorage.getItem('upicture'));
-    }
+    localforage.getItem('u_name').then(function (dat) {
+        $$('#u_name').html(dat);
+    });
+    localforage.getItem('u_phone').then(function (dat) {
+        $$('#u_phone').html(dat);
+    });
+    localforage.getItem('u_birthdate').then(function (dat) {
+        $$('#u_birthdate').html(dat);
+    });
+    localforage.getItem('u_address').then(function (dat) {
+        $$('#u_address').html(dat);
+    });
+    localforage.getItem('u_bloodtype').then(function (dat) {
+        $$('#u_bloodtype').html(dat);
+    });
+    localforage.getItem('u_medications').then(function (dat) {
+        $$('#u_medications').html(dat);
+    });
+    localforage.getItem('u_allergies').then(function (dat) {
+        $$('#u_allergies').html(dat);
+    });
+    localforage.getItem('u_medicalconditions').then(function (dat) {
+        $$('#u_medicalconditions').html(dat);
+    });
+    localforage.getItem('u_doctorinfo').then(function (dat) {
+        $$('#u_doctorinfo').html(dat);
+    });
+    localforage.getItem('u_emergencycontact_name').then(function (dat) {
+        $$('#u_emergencycontact_name').html(dat);
+    });
+    localforage.getItem('u_emergencycontact_phone').then(function (dat) {
+        $$('#u_emergencycontact_phone').html(dat);
+    });
+    localforage.getItem('u_usernotes').then(function (dat) {
+        $$('#u_usernotes').html(dat);
+    });
+    localforage.getItem('u_picture').then(function (pic) {
+        if (pic) {
+            $$('#u_picture').attr('src', pic);
+        } else {
+            $$('#u_picture').attr('src', 'img/account_96.png');
+        }
+    });
 }
 
 //Uploads an image to the specified path
@@ -108,26 +141,58 @@ function uploadImage(loc, img, callback = undefined) {
 }
 
 function loadSettings() {
-    let uinf = JSON.parse(localStorage.getItem('uinfo'));
-    if (uinf) {
-        $$('input[name=setting_name').val(uinf.name);
-        $$('input[name=setting_birthdate').val(uinf.birthdate);
-        $$('input[name=setting_phone').val(uinf.phone);
-        $$('input[name=setting_address').val(uinf.address);
-        $$('input[name=setting_bloodtype').val(uinf.bloodtype);
-        $$('input[name=setting_medications').val(uinf.medications);
-        $$('input[name=setting_allergies').val(uinf.allergies);
-        $$('input[name=setting_medicalconditions').val(uinf.medicalconditions);
-        $$('input[name=setting_doctorinfo').val(uinf.doctorinfo);
-        $$('input[name=setting_emergencycontact_name').val(uinf.emergencycontact_name);
-        $$('input[name=setting_emergencycontact_phone').val(uinf.emergencycontact_phone);
-        $$('input[name=setting_usernotes').val(uinf.usernotes);
-        $$('#setting_disp_picture').attr('src', localStorage.getItem('upicture'));
-    }
+    localforage.getItem('u_name').then(function (dat) {
+        $$('input[name=setting_name').val(dat);
+    });
+    localforage.getItem('u_phone').then(function (dat) {
+        $$('input[name=setting_phone').val(dat);
+    });
+    localforage.getItem('u_birthdate').then(function (dat) {
+        $$('input[name=setting_birthdate').val(dat);
+    });
+    localforage.getItem('u_address').then(function (dat) {
+        $$('input[name=setting_address').val(dat);
+    });
+    localforage.getItem('u_bloodtype').then(function (dat) {
+        $$('input[name=setting_bloodtype').val(dat);
+    });
+    localforage.getItem('u_medications').then(function (dat) {
+        $$('input[name=setting_medications').val(dat);
+    });
+    localforage.getItem('u_allergies').then(function (dat) {
+        $$('input[name=setting_allergies').val(dat);
+    });
+    localforage.getItem('u_medicalconditions').then(function (dat) {
+        $$('input[name=setting_medicalconditions').val(dat);
+    });
+    localforage.getItem('u_doctorinfo').then(function (dat) {
+        $$('input[name=setting_doctorinfo').val(dat);
+    });
+    localforage.getItem('u_emergencycontact_name').then(function (dat) {
+        $$('input[name=setting_emergencycontact_name').val(dat);
+    });
+    localforage.getItem('u_emergencycontact_phone').then(function (dat) {
+        $$('input[name=setting_emergencycontact_phone').val(dat);
+    });
+    localforage.getItem('u_usernotes').then(function (dat) {
+        $$('input[name=setting_usernotes').val(dat);
+    });
+    localforage.getItem('u_picture').then(function (pic) {
+        if (pic) {
+            $$('#setting_disp_picture').attr('src', pic);
+        } else {
+            $$('#setting_disp_picture').attr('src', 'img/account_96.png');
+        }
+
+    });
 }
 
-function getPhoneNumber() {
-    return JSON.parse(localStorage.getItem('uinfo')).phone;
+function getPhoneNumber(callback) {
+    localforage.getItem('u_phone').then(function (value) {
+        callback(value);
+    }).catch(function (err) {
+        app.alert('There was an error attempting to retrieve local data.', 'Error');
+    });
 }
 
 function saveSettings() {
@@ -145,10 +210,33 @@ function saveSettings() {
         emergencycontact_phone: $$('input[name=setting_emergencycontact_phone').val(),
         usernotes: $$('input[name=setting_usernotes').val()
     };
-    firebase.database().ref('users/' + uobj.phone).update(uobj);
-    localStorage.setItem('uinfo', JSON.stringify(uobj));
-    app.addNotification({ message: 'Saved Info.' });
-    goBack();
+    localforage.setItem('u_name', uobj.name).then(function () {
+        localforage.setItem('u_birthdate', uobj.birthdate).then(function () {
+            localforage.setItem('u_phone', uobj.phone).then(function () {
+                localforage.setItem('u_address', uobj.address).then(function () {
+                    localforage.setItem('u_bloodtype', uobj.bloodtype).then(function () {
+                        localforage.setItem('u_medications', uobj.medications).then(function () {
+                            localforage.setItem('u_allergies', uobj.allergies).then(function () {
+                                localforage.setItem('u_medicalconditions', uobj.medicalconditions).then(function () {
+                                    localforage.setItem('u_doctorinfo', uobj.doctorinfo).then(function () {
+                                        localforage.setItem('u_emergencycontact_name', uobj.emergencycontact_name).then(function () {
+                                            localforage.setItem('u_emergencycontact_phone', uobj.emergencycontact_phone).then(function () {
+                                                localforage.setItem('u_usernotes', uobj.usernotes).then(function () {
+                                                    firebase.database().ref('users/' + uobj.phone).update(uobj);
+                                                    app.addNotification({ message: 'Saved Info.' });
+                                                    goBack();
+                                                });
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
 
 //Checks if a files size is within the max limit
@@ -169,42 +257,39 @@ function updatePicture() {
         app.alert('Please select an image before uploading.', 'Select Image');
     }
     reader.onloadend = function () {
-        uploadImage('users/' + getPhoneNumber() + '/picture.jpg', reader.result, function (profurl) {
-            firebase.database().ref('users/' + getPhoneNumber()).update({ picture: profurl });
-            $$('#setting_disp_picture').attr('src', profurl);
-            localStorage.setItem('upicture', profurl);
+        getPhoneNumber(function (unum) {
+            uploadImage('users/' + unum + '/picture.jpg', reader.result, function (profurl) {
+                firebase.database().ref('users/' + unum).update({ picture: profurl });
+                $$('#setting_disp_picture').attr('src', profurl);
+                localforage.setItem('u_picture', profurl);
+            });
         });
+
     }
 }
 
-function onSuccess(position) {
-    var element = document.getElementById('geolocation');
-    element.innerHTML = 'Latitude: '  + position.coords.latitude      + '<br />' +
-                        'Longitude: ' + position.coords.longitude     + '<br />' +
-                        '<hr />'      + element.innerHTML;
-}
+var onSuccess = function (position) {
+    getPhoneNumber(function (unum) {
+        firebase.database().ref('users/' + unum).update({ latitude: position.coords.latitude, longitude: position.coords.longitude, altitude: position.coords.altitude, timestamp: position.timestamp })
+        app.addNotification({ message: "Updated Location" });
+    });
+};
 
 // onError Callback receives a PositionError object
 //
 function onError(error) {
-    alert('code: '    + error.code    + '\n' +
-          'message: ' + error.message + '\n');
+    app.alert('There was an error attempting to make the call.', 'Error');
 }
 
-// Options: throw an error if no update is received every 30 seconds.
-//
-var watchID;
-
 function contactEmergencyServices() {
-    //Place the call
-    //window.open(emergencyservicesnumber, '_system', 'location=yes');
-    //Upload latest user data
-    //let uobj = JSON.parse(localStorage.getItem('uinfo'));
-    //firebase.database().ref('users/' + uobj.phone).update(uobj);
-    //Mark user as active call
-    //firebase.database().ref('activecalls/' + getPhoneNumber()).set({ name: uobj.name, picture: localStorage.getItem('upicture') });
-    //Begin sending live location
-    watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+    localforage.getItem('u_name').then(function (uname) {
+        getPhoneNumber(function (unum) {
+            localforage.getItem('u_picture').then(function (pic) {
+                firebase.database().ref('activecalls/' + unum).set({ name: uname, picture: pic });
+                window.open(emergencyservicesnumber, '_system', 'location=yes');
+            });
+        });
+    });
 }
 /* ========= End Functions ======= */
 
